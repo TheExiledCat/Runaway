2da65172dbd213bc16e9ec42885ac7dace3ed454
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-03-21 17:27:23 +0100

    Added objects and scripts
    Player_movement handled, prefabs for all objects set, materials.

---
+++ Runaway/Assets/scripts/Player_movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public LayerMask whatisGround;
+    public Transform floorcheck;
+    float checkRadi =0.1f;
+    float strafeSpeed =50;
+    float walkSpeed =100;
+    float jumpHeight = 7;
+    bool isGrounded;
+    float runSpeed;
+    float moveInX;
+    float moveInZ;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(floorcheck.position, checkRadi,whatisGround);
+        moveInZ = Input.GetAxis("Horizontal");
+        moveInX = Input.GetAxis("Vertical");
+        if (isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);
+        }
+        rb.velocity = new Vector3(moveInX * strafeSpeed, rb.velocity.y, -moveInZ * walkSpeed);
+    }
+}
