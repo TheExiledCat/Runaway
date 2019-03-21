5b8ca5b6637c50fdca08f208763adc8b9e896055
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-03-22 00:23:53 +0100

    added comments

--- Runaway/Assets/scripts/Player_movement.cs
+++ Runaway/Assets/scripts/Player_movement.cs
@@ -3,32 +3,50 @@
 public class Player_movement : MonoBehaviour
 {
     Rigidbody rb;
-    public LayerMask whatisGround;
-    public Transform floorcheck;
-    float checkRadi =0.1f;
-    float strafeSpeed =50;
-    float walkSpeed =100;
-    float jumpHeight = 7;
-    bool isGrounded;
+    public LayerMask whatisGround; // var that keeps track of what you want to be able to jump off of
+    public Transform floorcheck; //hitbox for ground check
+    float checkRadi =0.1f; // size of hitbox groundcheck
+    float strafeSpeed =50; // left and right speed
+    float walkSpeed =100; //moving speed
+    float jumpHeight = 7; //jump height
+    bool isGrounded; // boolean that keeps track if we are touching something qualified as "ground" or not
     float runSpeed;
-    float moveInX;
+    float moveInX; // vvv //take in all inputs
     float moveInZ;
+    float lookX;
+    float lookS = 20;
+    float lookY;
+
+    public Camera cam;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(floorcheck.position, checkRadi,whatisGround);
+        lookY = Input.GetAxis("Mouse Y");
+        lookX = Input.GetAxis("Mouse X"); //set vars equal to our input
+        isGrounded = Physics.CheckSphere(floorcheck.position, checkRadi,whatisGround); // check for collision at hitbox position
         moveInZ = Input.GetAxis("Horizontal");
-        moveInX = Input.GetAxis("Vertical");
+        moveInX = Input.GetAxis("Vertical");//set vars equal to input from mouse
         if (isGrounded && Input.GetKey(KeyCode.Space))
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);
+            rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);//jumping
         }
-        rb.velocity = new Vector3(moveInX * strafeSpeed, rb.velocity.y, -moveInZ * walkSpeed);
+        
+
+
+    
+   
+    }
+    private void FixedUpdate()
+    {
+
+        
+          
+         rb.velocity += new Vector3(moveInX * strafeSpeed, rb.velocity.y, -moveInZ * walkSpeed);//moving
     }
 }
